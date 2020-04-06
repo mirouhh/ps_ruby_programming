9e7a0901e45eb4f323398f2ee9d95fa7e264b0c3
mirouhh <mirouhh@googlemail.com>
2020-04-06 21:54:12 +0200

    [Exercise 4.3] Adding new player and display its name in upcase.

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,4 +1,9 @@
 name1 = "larry"
 health1 = 60
 
-puts "#{name1.capitalize} has a health of #{health1}"
+puts "#{name1.capitalize} has a health of #{health1}."
+
+name2 = "curly"
+health2 = 125
+
+puts "#{name2.upcase} has a health of #{health2}."
