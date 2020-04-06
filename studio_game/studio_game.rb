9d753fd36dc766d0df4ce16ebe8db519b22750af
mirouhh <mirouhh@googlemail.com>
2020-04-06 22:01:59 +0200

    [Exercise 4.6] Adding a new player and an new way of formating the output.

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -12,3 +12,11 @@ health1 = 30
 
 puts "#{name1.capitalize} has a health of #{health1}."
 puts "#{name2.upcase} has a health of #{health2}."
+
+name3 = "moe"
+health3 = 100
+
+text = "#{name3.capitalize} has a health of #{health3}."
+puts text.center(50, '*')
+
+puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
