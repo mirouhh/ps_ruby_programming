9334301ca9a1f8bdce2fbea8dd4a6cf6dfc681d2
mirouhh <mirouhh@googlemail.com>
2020-04-06 21:48:35 +0200

    [Bonus 3.2] Adding more funded projects and displaying them

--- crowdfund/crowdfund.rb
+++ crowdfund/crowdfund.rb
@@ -2,3 +2,10 @@ project1 = "ABC"
 funding1 = "$1000"
 
 puts "#{project1} has #{funding1} in funding."
+
+puts ""
+
+project2 = "LMN"
+project3 = "XYZ"
+
+puts "Projects: \n\t#{project1}\n\t#{project2}\n\t#{project3}"
