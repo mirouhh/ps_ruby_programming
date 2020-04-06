4b1ddd87d7d3312efe5ae790b405ad64e624a822
mirouhh <mirouhh@googlemail.com>
2020-04-06 21:57:45 +0200

    [Exercise 4.2.3] Player1 gets a new health. Printing out data of both players to check.

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -7,3 +7,8 @@ name2 = "curly"
 health2 = health1
 
 puts "#{name2.upcase} has a health of #{health2}."
+
+health1 = 30
+
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
