ba1731985b34b53db658456050baa051ac03fcfd
mirouhh <mirouhh@googlemail.com>
2020-04-06 22:13:51 +0200

    [Bonus 4.2] Adding fundings to project2 and project3.

--- crowdfund/crowdfund.rb
+++ crowdfund/crowdfund.rb
@@ -6,6 +6,8 @@ puts "#{project1} has #{funding1} in funding."
 puts ""
 
 project2 = "LMN"
+funding2 = "$1250"
 project3 = "XYZ"
+funding3 = "$2250"
 
 puts "Projects: \n\t#{project1}\n\t#{project2}\n\t#{project3}"
