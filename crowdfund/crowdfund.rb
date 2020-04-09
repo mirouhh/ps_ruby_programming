362a4eee66458ef42c33036c44a7edddbfcaa984
mirouhh <mirouhh@googlemail.com>
2020-04-09 21:50:14 +0200

    [Bonus 7.2] Update the crowdfunding code to make use of classes and methods.

--- crowdfund/crowdfund.rb
+++ crowdfund/crowdfund.rb
@@ -1,7 +1,33 @@
-def has_funding(project, funding=1250)
-	"#{project} has $#{funding} in funding."
+class Project
+
+	def initialize(name, funding, target_funding)
+		@name = name.upcase
+		@funding = funding
+		@target_funding = target_funding
+	end
+
+	def add_funding(amount)
+		@funding += amount
+		"Project #{@name} got more funds!"
+	end
+
+	def remove_funding(amount)
+		@funding -= amount
+		"Project #{@name} lost some funds!"
+	end
+
+	def to_s
+		"Project #{@name} has $#{@funding} in funding towards a goal of $#{@target_funding}."
+	end
+
 end
 
-puts has_funding("ABC", 1000)
-puts has_funding("LMN")
-puts has_funding("XYZ", 1250)
+project1 = Project.new("lmn", 500, 3000)
+project2 = Project.new("xyz", 25, 75)
+
+puts project1
+puts project2
+puts project1.remove_funding(15)
+puts project2.add_funding(25)
+puts project1
+puts project2
