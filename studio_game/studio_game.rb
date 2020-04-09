cc2aa233ffc882254354e197abfcaeb0658e3df4
mirouhh <mirouhh@googlemail.com>
2020-04-09 21:15:11 +0200

    [Exercise 7.2.5] Adjusting the initializer to set the default health to 100.

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,10 +1,10 @@
 class Player
-	def initialize(name, health)
+	def initialize(name, health=100)
 		@name = name.capitalize
 		@health = health
 	end
 end
 
-player1 = Player.new("moe", 100)
+player1 = Player.new("moe")
 
 puts player1.inspect
